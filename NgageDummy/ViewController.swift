fafdc040450e06ae8f90fa40d1563acf3e74430e
Ayush Sharma <77ayushsharma77@gmail.com>
2019-11-07 21:23:20 +0530

    Initial Commit

---
+++ NgageDummy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NgageDummy
+//
+//  Created by Ayush Sharma on 07/11/19.
+//  Copyright © 2019 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
